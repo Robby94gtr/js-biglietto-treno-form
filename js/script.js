aeb56808501070f87135a7a2f8b410a4e17860da
Robby94gtr <robertogramazio94@gmail.com>
2025-04-01 01:53:12 +0200

    [UPD] Declared all variables in script.js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Dichiaro tutte le variabili che mi servono
+
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+const form = document.querySelector('form');
+
+const userName = document.getElementById('userName');
+const userKm = document.getElementById('userKm');
+const userAge = document.getElementById('userAge');
+
+const offerta = document.getElementById('offerta');
+const carrozza = document.getElementById('carrozza');
+const codice = document.getElementById('codice');
+const costo = document.getElementById('costo');
+const passName = document.getElementById('pass-name');
