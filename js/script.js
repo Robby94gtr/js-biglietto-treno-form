ceb8697cc0532cdebb35a51e665c499fb696b412
Robby94gtr <robertogramazio94@gmail.com>
2025-04-01 01:58:11 +0200

    [UPD] Implemented function of generate button in javascript

--- js/script.js
+++ js/script.js
@@ -14,3 +14,28 @@ const carrozza = document.getElementById('carrozza');
 const codice = document.getElementById('codice');
 const costo = document.getElementById('costo');
 const passName = document.getElementById('pass-name');
+
+//creo funzionamento del bottone "genera"
+
+button1.addEventListener('click', function(event) {
+    event.preventDefault();
+    carrozza.innerHTML = Math.floor(Math.random() * 9) + 1; 
+    codice.innerHTML = Math.floor(Math.random() * 10000) + 1; 
+    passName.innerHTML = userName.value; 
+    
+
+    let prezzo = 0;
+    const costoKm = 0.21; 
+    prezzo = costoKm * parseFloat(userKm.value);
+
+    if (userAge.value === 'minorenne') { 
+        costo.innerHTML = (prezzo - (prezzo * 20 / 100)).toFixed(2);
+        offerta.innerHTML = 'Sconto minorenne';
+    } else if (userAge.value === 'over 65') { 
+        costo.innerHTML = (prezzo - (prezzo * 40 / 100)).toFixed(2);
+        offerta.innerHTML = 'Sconto over 65';
+    } else {
+        costo.innerHTML = prezzo.toFixed(2);
+        offerta.innerHTML = 'Tariffa standard';
+    }
+});
